c319090810f788149dd38682cd248c102879b77f
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2021-04-30 12:53:12 +0200

    add issues array and show content in list page

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Issue } from '../issue';
 
 @Component({
   selector: 'app-issue-list',
@@ -6,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./issue-list.component.css']
 })
 export class IssueListComponent implements OnInit {
+
+  public issues: Issue[] = [
+    {
+      id: 1,
+      title: 'The issue #1',
+      description: 'Fatallica',
+      place: 'PC-42',
+      status: 'NEW',
+    },
+    {
+      id: 2,
+      title: 'MegaIssue',
+      description: 'MegaFatal',
+      place: 'PC-12',
+      status: 'DOING',
+    },
+    {
+      id: 3,
+      title: 'Unknown issue',
+      description: 'U.F.O',
+      place: 'PC-01',
+      status: 'DOING',
+    },
+    {
+      id: 4,
+      title: 'Broken Windows 10',
+      description: 'Everywhere',
+      place: 'PC-666',
+      status: 'DONE'
+    },
+  ]
 
   constructor() { }
 
---
+++ src/app/issue.ts
@@ -0,0 +1,7 @@
+export class Issue {
+  id: number = 0;
+  title: string = '';
+  description: string = '';
+  place: string = '';
+  status: string = '';
+}
